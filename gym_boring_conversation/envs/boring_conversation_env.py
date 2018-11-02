89f02892a6415f44f54e790aff318e515784ba0d
sveng1 <selina.veng@gmail.com>
2018-11-02 14:32:48 +0100

    update env

--- gym_boring_conversation/envs/boring_conversation_env.py
+++ gym_boring_conversation/envs/boring_conversation_env.py
@@ -8,16 +8,15 @@ class Boring_conversationEnv(gym.Env):
 
     def __init__(self):
 
-        self.is_person_angry = 0
+        self.state = 0
         self.action_space = spaces.Discrete(2)
+        self.observation_space = spaces.Discrete(2)
         self.done = False
         self.info = "No info right now"
 
-
     def _transition_prob(self,a):
 
-        s = self.is_person_angry
-
+        s = self.state
         p = {}
         #p[s,a]={(r,s'):P(r,s'|s,a)}
         p[(0,0)] = {(0,0):0.7, (-2,1):0.3}
@@ -26,7 +25,6 @@ class Boring_conversationEnv(gym.Env):
         p[(1,1)] = {(-5,0):1}
 
         return p[(s,a)]
-
 
     def step(self, action):
 
@@ -38,14 +36,13 @@ class Boring_conversationEnv(gym.Env):
         step = trans[outcome[0]]
         reward, s_next = step
     
-        self.is_person_angry = s_next
+        self.state = s_next
         
         return s_next, reward, self.done, info
 
     def reset(self):
 
-        self.is_person_angry = 0
-        return "The boring person is happy."
+        self.state = 0
 
     
     def render(self, mode='human', close=False):
