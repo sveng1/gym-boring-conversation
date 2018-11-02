f2637dcea92c9640eecc8285e0b22ee6d505ef62
sveng1 <selina.veng@gmail.com>
2018-11-02 13:29:57 +0100

    remove extra env

--- gym_boring_conversation/__init__.py
+++ gym_boring_conversation/__init__.py
@@ -4,7 +4,3 @@ register(
     id='boring_conversation-v0',
     entry_point='gym_boring_conversation.envs:Boring_conversationEnv',
 )
-register(
-    id='boring_conversation-extrahard-v0',
-    entry_point='gym_boring_conversation.envs:Boring_conversationExtraHardEnv',
-)
