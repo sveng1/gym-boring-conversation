fc0e7f633e86cb4a101a851ddf8404ef30ca2404
sveng1 <selina.veng@gmail.com>
2018-11-02 13:17:22 +0100

    defining env

--- gym_boring_conversation/envs/boring_conversation_env.py
+++ gym_boring_conversation/envs/boring_conversation_env.py
@@ -1,15 +1,37 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+import numpy as np
+
 
 class Boring_conversationEnv(gym.Env):
-  metadata = {'render.modes': ['human']}
+    metadata = {'render.modes': ['human']}
 
-  def __init__(self):
-    ...
-  def step(self, action):
-    ...
-  def reset(self):
-    ...
-  def render(self, mode='human', close=False):
-    ...
+    def __init__(self):
+
+        self.is_person_angry = 0
+        self.action_space = spaces.Discrete(2)
+
+    def transition_prob(a):
+
+        s = self.is_person_angry
+
+        p = {}
+        #p[s,a]={(r,s'):P(r,s'|s,a)}
+        p[(0,0)] = {(0,0):0.7, (-2,1):0.3}
+        p[(0,1)] = {(-5,0):1}
+        p[(1,0)] = {(-2,1): 0.95, (0,0):0.05}
+        p[(1,1)] = {(-5,0):1}
+
+        return p[(s,a)]
+
+
+    def step(self, action):
+
+    
+        return reward, s_next
+
+
+    def get_reward(self):
+    
+    def render(self, mode='human', close=False):
