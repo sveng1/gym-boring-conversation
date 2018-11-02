6affa8f112180fc401708f385c7ea7eaf8647a7e
sveng1 <selina.veng@gmail.com>
2018-11-02 11:16:15 +0100

    structure

--- gym_boring_conversation/__init__.py
+++ gym_boring_conversation/__init__.py
@@ -2,9 +2,9 @@ from gym.envs.registration import register
 
 register(
     id='boring_conversation-v0',
-    entry_point='gym_boring_conversation.envs:boring_conversationEnv',
+    entry_point='gym_boring_conversation.envs:Boring_conversationEnv',
 )
 register(
     id='boring_conversation-extrahard-v0',
-    entry_point='gym_boring_conversation.envs:boring_conversationExtraHardEnv',
+    entry_point='gym_boring_conversation.envs:Boring_conversationExtraHardEnv',
 )
